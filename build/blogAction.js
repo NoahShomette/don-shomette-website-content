0c5e6a13d311e6152c101338d25d6aabf8d8846d
Noah <noahshomette@gmail.com>
2024-11-22 23:47:07 -0500

    Put blogs in individual folders, render blog and save in html file, add photos to this repo. Make a release action.

--- build/blogAction.js
+++ build/blogAction.js
@@ -4,39 +4,104 @@ var fs = require('fs');
 var path = require('path');
 const jsYaml = require('js-yaml');
 const grayMatter = require('gray-matter');
+const { Remarkable } = require('remarkable');
+const { blogContentRenderer } = require('./blogContentRenderer');
 
 var blogsJson = { "config": { version: 1 }, "blogs": [] };
+var foldersNeedingRenamed = { "folders": [] };
 
-fs.readdir("blogs", function (err, files) {
-    if (err) {
+const md = new Remarkable('commonmark');
+md.use(blogContentRenderer);
+
+let blogFolder;
+try {
+    blogFolder = fs.readdirSync("blogs");
+} catch (err) {
+    console.error("Could not list the directory.", err);
+    process.exit(1);
+}
+
+blogFolder.forEach(function (folder, index) {
+    let folderPath = path.join("blogs/", folder);
+    // Read every file in the blog folder
+    let blogFolderFiles;
+    try {
+        blogFolderFiles = fs.readdirSync(folderPath);
+    } catch (err) {
         console.error("Could not list the directory.", err);
         process.exit(1);
     }
+    let blogsJsonListing = {};
+    let goalFolderPath;
+    let photoFile;
+    blogFolderFiles.forEach(function (file, index) {
+        let actualFilePath = path.join(folderPath, file);
+        let fileEnding = getFileEnding(actualFilePath);
+        console.log(actualFilePath);
+        // If its the markdown file then parse it and get the info it needs as well as render the html
+        if (fileEnding === "md") {
+            let file;
+            try {
+                file = fs.readFileSync(actualFilePath, "utf8");
+            } catch (err) {
 
-    files.forEach(function (file, index) {
-
-        var filePath = path.join("blogs/", file);
-
-        fs.stat(filePath, function (error, stat) {
-            if (error) {
-                console.error("Error stating file.", error);
-                return;
             }
-
-            if (!stat.isFile()) {
-                console.error("Could not open file");
-                process.exit(1);
-            }
-
-        });
-
-        fs.readFile(filePath, function (error, file) {
             let parsedFile = grayMatter(file)
             let frontmatter = jsYaml.load(parsedFile.matter)
-            blogsJson.blogs.push({ title: frontmatter.title, date: frontmatter.date, photoLink: frontmatter.photoLink, link: frontmatter.title.replace(/[^\w ]/g, ' ').trim().replace(/[^\w]/g, '-').replace(/(-)(?=-{0,}\1)/g, "").toLowerCase() })
-            fs.writeFile("blogs.json", JSON.stringify(blogsJson), err => {
+            console.log(frontmatter);
+            blogsJsonListing.title = frontmatter.title;
+            blogsJsonListing.date = frontmatter.date;
+            blogsJsonListing.link = blogLinkName(frontmatter.title);
+            goalFolderPath = path.join("blogs/", blogsJsonListing.link)
+            // Write the rendered content to a html file
+            try { fs.writeFileSync(path.join(folderPath, "blogContent.html"), md.render(parsedFile.content)) }
+            catch (err) {
+                console.error("failed to write rendered blog to file", err);
+                process.exit(1);
+            }
+        };
+        // If its an image then its our blog photos
+        if (fileEnding === "png" || fileEnding === "jpg" || fileEnding === "jpeg" || fileEnding === "webp") {
+            photoFile = file;
+        }
+    });
+    if (photoFile != null && goalFolderPath != null) {
+        blogsJsonListing.photoLink = path.join("https://raw.githubusercontent.com/NoahShomette/don-shomette-website-content/release/", goalFolderPath, photoFile);
+    }
+    // After we've read all the files go ahead and do whatever we need to
+    console.log(blogsJsonListing);
+    // verify that the listing has everything we need and then push it to the blogsJson
+    if (blogsJsonListing.title != null && blogsJsonListing.date != null && blogsJsonListing.photoLink != null && blogsJsonListing.link != null) {
+        blogsJson.blogs.push(blogsJsonListing);
+    } else {
+        console.error("Listing for blogsJson did not include all required fields for blog", folderPath);
+        process.exit(1);
+    }
 
-            });
-        });
+    if (folderPath != goalFolderPath) {
+        foldersNeedingRenamed.folders.push({ oldFolderPath: folderPath, goalFolderPath: goalFolderPath });
+    }
+});
+
+foldersNeedingRenamed.folders.forEach(function (folderInfo, index) {
+    fs.rename(folderInfo.oldFolderPath, folderInfo.goalFolderPath, function (err) {
+        if (err) console.log('ERROR: ' + err);
     });
 });
+
+try { fs.writeFileSync("blogs.json", JSON.stringify(blogsJson)) }
+catch (err) {
+    console.error("failed to write blogs.json", err);
+    process.exit(1);
+}
+
+function blogLinkName(title) {
+    return title.replace(/[^\w ]/g, ' ').trim().replace(/[^\w]/g, '-').replace(/(-)(?=-{0,}\1)/g, "").toLowerCase()
+}
+
+function getFileEnding(filePath) {
+    var fileEnding = filePath.split(".");
+    fileEnding = fileEnding.at(fileEnding.length - 1);
+    return fileEnding
+}
+//https://raw.githubusercontent.com/NoahShomette/don-shomette-website-content/release/
---
+++ build/blogContentRenderer.js
@@ -0,0 +1,85 @@
+﻿
+heading_open = function (tokens, idx /*, options, env */) {
+    switch (tokens[idx].hLevel) {
+        case 1: {
+            return '<h1 class="dark mt-15 f-display-small font-secondary ls-2">';
+        }
+        case 2: {
+            return '<h2 class="mt-15 f-headline-large font-secondary dark ls-1">';
+        }
+        case 3: {
+            return '<h3 class="mt-15 f-headline-medium bold dark">';
+        }
+        case 4: {
+            return '<h4 class="mt-15 f-headline-small bold">';
+        }
+        case 5: {
+            return '<h5 class="mt-15 f-title-large bold">';
+        }
+        case 6: {
+            return '<h6 class="mt-15 f-title-medium bold">';
+        }
+    }
+}
+
+paragraph_open = function (tokens, idx) {
+    return tokens[idx].tight ? '' : '<p class="f-body-large gray9 mt-15">';
+}
+
+bullet_list_open = function () {
+    return '<ul class="list list-lg list-disc mt-30 gray7 f-body-large dark">\n';
+}
+
+ordered_list_open = function (tokens, idx) {
+    const token = tokens[idx];
+    const order = token.order > 1 ? ' start="' + token.order + '"' : '';
+    return '<ol' + order + ' class="list-number list list-lg list-disc mt-30 gray7 f-body-large dark">\n';
+}
+
+link_open = function (tokens, idx, options /* env */) {
+    let href = tokens[idx].href;
+    const title = tokens[idx].title ? (' title="' + tokens[idx].title + '"') : '';
+    const target = options.linkTarget ? (' target="' + options.linkTarget + '"') : '';
+    return '<a class="colored underline-hover" href="' + href + '"' + title + target + '>';
+};
+
+link_close = function (/* tokens, idx, options, env */) {
+    return '</a>';
+};
+
+var stack = [];
+
+blockquote_open = function (state) {
+    for (var i = 0; i < state.tokens.length; i++) {
+        var tok = state.tokens[i];
+        if (tok.type === 'blockquote_open') {
+            state.tokens.splice(i, 1);
+            state.tokens.splice(i, 0, { type: 'htmltag', content: '<blockquote class="bg-gray2 p-2 mt-15 lh-lg bl-3 f-title-large b-colored">', level: 0 });
+            stack.push(tok);
+
+        } else if (tok.type === 'blockquote_close') {
+            stack.pop();
+        }
+
+        else if (stack.length && tok.type === 'inline') {
+            state.tokens.splice(i - 1, 1);
+            var last = tok.children.pop();
+            //state.tokens.splice(i, 0, { type: 'htmltag', content: '<p class="fs-24 bold">', level: 0 });
+            state.tokens.splice(i + 1, 0, { type: 'text', content: last.content, level: 0 });
+            //state.tokens.splice(i + 2, 0, { type: 'htmltag', content: '</p>', level: 0 });
+
+        }
+    }
+};
+
+module.exports = {
+    blogContentRenderer: function(md) {
+        md.renderer.rules.heading_open = heading_open;
+        md.renderer.rules.paragraph_open = paragraph_open;
+        md.renderer.rules.bullet_list_open = bullet_list_open;
+        md.renderer.rules.ordered_list_open = ordered_list_open;
+        md.renderer.rules.link_open = link_open;
+        md.renderer.rules.link_close = link_close;
+        md.core.ruler.push('custom_blockquote', blockquote_open);
+    }
+};
